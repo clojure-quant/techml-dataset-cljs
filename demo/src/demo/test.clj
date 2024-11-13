ea0a23370d7334f4e8120ef7ffec204a04104d0a
awb99 <awb99@gmail.com>
2024-11-13 07:18:50 -0500

    more test

--- demo/src/demo/test.clj
+++ demo/src/demo/test.clj
@@ -1,8 +1,12 @@
 (ns demo.test
- (:require
-  [tick.core :as t]
-  [tech.v3.dataset :as ds]
-  [cquant.tmlds :as qds]))
+  (:require
+   [tick.core :as t]
+   [tech.v3.dataset :as ds]
+   [cquant.tmlds :as qds]
+   [tech.v3.io :as io]
+   [tech.v3.libs.clj-transit :as tech-transit]
+    [tech.v3.datatype.datetime :as dtype-dt]
+   ))
 
 (defn demo-ds [n]
   (ds/->dataset
@@ -22,4 +26,99 @@ dt
 (qds/ds->transit-json-file dt ".webly/dt.transit-json")
 
 (qds/transit-json-file->ds ".webly/dt.transit-json")
+
+
+
+
+(defn ds->transit-json-file2
+  [ds fname]
+  (with-open [outs (io/output-stream! fname)]
+    (tech-transit/dataset->transit ds outs)))
+
+(defn transit-json-file->ds2
+  [fname]
+  (with-open [ins (io/input-stream fname)]
+    (tech-transit/transit->dataset ins)))
+
+(def dsdt
+  (ds/->dataset
+   {:dt [(t/instant)]}))
+
+dsdt
+;; => _unnamed [1 1]:
+;;    
+;;    |                         :dt |
+;;    |-----------------------------|
+;;    | 2024-11-13T02:42:49.219519Z |
+
+
+(ds->transit-json-file2 dsdt ".webly/dsdt2.transit-json")
+
+(transit-json-file->ds2 ".webly/dsdt2.transit-json")
+;; => _unnamed [1 1]:
+;;    
+;;    |                         :dt |
+;;    |-----------------------------|
+;;    | 1970-01-21T00:57:45.769219Z |
+
+
+
+(qds/ds->transit-json-file dsdt ".webly/dsdt.transit-json")
+
+(qds/transit-json-file->ds ".webly/dsdt.transit-json")
+
+
+
+
+
+(-> (t/instant "1970-01-21T00:57:45.769219Z")
+    (t/long))
+;; => 1731465
+
+
+(-> (t/instant "2024-11-13T02:42:49.219519Z")
+    (t/long))
+;; => 1731465769
+
+
+
+  (defn master-ds
+  []
+  (ds/->dataset {:a (mapv double (range 5))
+                 :b (repeat 5 :a)
+                 :c (repeat 5 "hey")
+                 :d (repeat 5 {:a 1 :b 2})
+                 :e (repeat 4 [1 2 3])
+                 :f (repeat 5 (dtype-dt/local-date))
+                 :g (repeat 5 (dtype-dt/instant))
+                 :h [true false true true false]
+                 :i (repeat 5 "text")
+                 :j [1 nil 2 nil 3]}
+                {:parser-fn {:i :text}}))
+
+(master-ds)
+;; => _unnamed [5 10]:
+;;    
+;;    |      :e |                          :g |  :c | :j |    :h | :b |           :d |         :f |   :i |  :a |
+;;    |---------|-----------------------------|-----|---:|-------|----|--------------|------------|------|----:|
+;;    | [1 2 3] | 2024-11-13T12:12:01.774336Z | hey |  1 |  true | :a | {:a 1, :b 2} | 2024-11-13 | text | 0.0 |
+;;    | [1 2 3] | 2024-11-13T12:12:01.774336Z | hey |    | false | :a | {:a 1, :b 2} | 2024-11-13 | text | 1.0 |
+;;    | [1 2 3] | 2024-11-13T12:12:01.774336Z | hey |  2 |  true | :a | {:a 1, :b 2} | 2024-11-13 | text | 2.0 |
+;;    | [1 2 3] | 2024-11-13T12:12:01.774336Z | hey |    |  true | :a | {:a 1, :b 2} | 2024-11-13 | text | 3.0 |
+;;    |         | 2024-11-13T12:12:01.774336Z | hey |  3 | false | :a | {:a 1, :b 2} | 2024-11-13 | text | 4.0 |
+
+
+
+(-> (master-ds)
+    (tech-transit/dataset->transit-str)
+    (tech-transit/transit-str->dataset))
+;; => _unnamed [5 10]:
+;;    
+;;    |      :e |                          :g |  :c | :j |    :h | :b |           :d |         :f |   :i |  :a |
+;;    |---------|-----------------------------|-----|---:|-------|----|--------------|------------|------|----:|
+;;    | [1 2 3] | 1970-01-21T00:58:19.924669Z | hey |  1 |  true | :a | {:a 1, :b 2} | 2024-11-13 | text | 0.0 |
+;;    | [1 2 3] | 1970-01-21T00:58:19.924669Z | hey |    | false | :a | {:a 1, :b 2} | 2024-11-13 | text | 1.0 |
+;;    | [1 2 3] | 1970-01-21T00:58:19.924669Z | hey |  2 |  true | :a | {:a 1, :b 2} | 2024-11-13 | text | 2.0 |
+;;    | [1 2 3] | 1970-01-21T00:58:19.924669Z | hey |    |  true | :a | {:a 1, :b 2} | 2024-11-13 | text | 3.0 |
+;;    |         | 1970-01-21T00:58:19.924669Z | hey |  3 | false | :a | {:a 1, :b 2} | 2024-11-13 | text | 4.0 |
 
