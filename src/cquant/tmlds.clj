bfc8bc26c91332fa2f8b36793182b9df7fe83d6f
awb99 <awb99@gmail.com>
2025-05-05 09:44:52 -0500

    vectorclone fix via dtype-next

--- src/cquant/tmlds.clj
+++ src/cquant/tmlds.clj
@@ -4,14 +4,16 @@
    [tech.v3.datatype.protocols]
    [tablecloth.api :as tc]))
 
+;; this fixes are solved with cnuernber/dtype-next {:mvn/version "10.140"} ; vector clone fix
+
 ;; this fixes a bug of cloning columns that are vectors
 ;; requires clojure 1.12
 ;; see:
 ;; https://github.com/cnuernber/dtype-next/issues/129
 
-(extend-type clojure.lang.APersistentVector/1
-  tech.v3.datatype.protocols/PClone
-  (clone [this] (aclone (to-array (vec this)))))
+;(extend-type clojure.lang.APersistentVector/1
+;  tech.v3.datatype.protocols/PClone
+;  (clone [this] (aclone (to-array (vec this)))))
 
 ;For clojure < 1.12:
 ;(extend-type (Class/forName "[Lclojure.lang.APersistentVector;")
